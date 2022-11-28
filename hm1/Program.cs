8467aede52f9a72181d3b32719981c4cc96239b4
timur <t.worck.m@gmail.com>
2022-11-28 10:40:29 +0100

    add inf in readme

--- hm1/Program.cs
+++ hm1/Program.cs
@@ -5,8 +5,8 @@
 a = 5; b = 7->max = 7
 a = 2 b = 10->max = 10
 a = -9 b = -3->max = -3
-*/
-/*
+
+
 Console.WriteLine("Введите два числа");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
@@ -25,17 +25,16 @@ else
     Console.WriteLine("Большее число: " + b);
     Console.WriteLine("Меньшее число: " + a);
 }
-*/
 
-/*
+
+
 Задача 4: Напишите программу, которая принимает на вход три числа 
 и выдаёт максимальное из этих чисел.
 
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22
-*/
-/*
+ 
 Console.WriteLine("Введите три числа");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
@@ -44,25 +43,25 @@ int c = Convert.ToInt32(Console.ReadLine());
 if (a >= b & a >= c) Console.WriteLine("Большее число: " + a);
 else if (b >= a & b >= c) Console.WriteLine("Большее число: " + b);
 else Console.WriteLine("Большее число: " + c);
-*/
 
-/*
+
+
 Задача 6: Напишите программу, которая на вход принимает число 
 и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-4 -> да
--3 -> нет
-7 -> нет
-*/
-/*
+4->да
+- 3->нет
+7->нет
+
+
 Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
 
 if (a % 2 == 0) Console.Write("да");
 else Console.Write("нет");
-*/
 
-/*
+
+
 Задача 8: Напишите программу, которая на вход принимает число (N), 
 а на выходе показывает все чётные числа от 1 до N.
 
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
+dotnet new console - настройка папки для работы с C#
+dotnet run - выполнение программы
+
+Через console мы обращаемся к системному терминалу
+
 В Windows Shift+ Alt+ F
 На Mac Shift+ Option+ F
 В Linux Ctrl+ Shift+ I
