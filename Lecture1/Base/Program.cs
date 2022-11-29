b542b56722874c8bbd68491c02266fa37f68654b
timur <t.worck.m@gmail.com>
2022-11-29 13:48:47 +0100

    add rezalut

--- Lecture1/Base/Program.cs
+++ Lecture1/Base/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 5;
+int c = 8;
+int d = 3;
+int e = 2;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+System.Console.WriteLine($"max = {max}");
