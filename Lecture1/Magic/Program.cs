08ddf3fb0278398bf27532bbfb97b4acc9dfa9d8
timur <t.worck.m@gmail.com>
2022-11-29 14:14:04 +0100

    add program code

--- Lecture1/Magic/Program.cs
+++ Lecture1/Magic/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+/*
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
+*/
+
+int xa = 0, ya = 1;
+int xb = 0, yb = 3;
+int xc = 6, yc = 4;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+//Console.WriteLine("+");
