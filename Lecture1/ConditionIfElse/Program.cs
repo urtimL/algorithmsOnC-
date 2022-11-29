7a1acd30bf6d146ba7a84863c5bc474166d70e79
timur <t.worck.m@gmail.com>
2022-11-29 13:33:37 +0100

    add code

--- Lecture1/ConditionIfElse/Program.cs
+++ Lecture1/ConditionIfElse/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("Введите имя пользователя");
+string? userName = Console.ReadLine();
+
+if (userName == null) userName = "";
+
+if (userName.ToLower() == "маша") Console.WriteLine("Ура! Это же Маша!");
+else Console.WriteLine($"Привет {userName}");
