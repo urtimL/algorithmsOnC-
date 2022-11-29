7ac4d585f9cd9f0f2bd4ad0dbbe1ed962740b523
timur <t.worck.m@gmail.com>
2022-11-29 15:37:42 +0100

    add program Magic

--- Lecture1/Magic/Program.cs
+++ Lecture1/Magic/Program.cs
@@ -1,12 +1,8 @@
-﻿Console.Clear();
-/*
-Console.SetCursorPosition(10, 4);
-Console.WriteLine("+");
-*/
+﻿int xa = 60, ya = 0;
+int xb = 0, yb = 40;
+int xc = 120, yc = 40;
 
-int xa = 0, ya = 1;
-int xb = 0, yb = 3;
-int xc = 6, yc = 4;
+Console.Clear();
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -15,4 +11,32 @@ Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yc);
-//Console.WriteLine("+");
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+int count = 0, i = 0;
+
+while (count <= 10000)
+{
+    i = new Random().Next(0, 3);
+    if (i == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    else if (i == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    else
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+
+    count++;
+}
