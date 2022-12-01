f4bd51ee9975d3fa897e6426d7bfda9b1782d760
timur <t.worck.m@gmail.com>
2022-12-01 17:40:31 +0100

    add Max code

--- Lecture1/Max/Program.cs
+++ Lecture1/Max/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (result < arg2) result = arg2;
+    if (result < arg3) result = arg3;
+
+    return result;
+}
+
+int a1 = 15, b1 = 8, c1 = 5;
+int a2 = 1, b2 = 18, c2 = 25;
+int a3 = 11, b3 = 81, c3 = 51;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+int max = Max(max1, max2, max3);
+
+System.Console.WriteLine(max);
