63983958332d8238648d6614f6aa4165aea6853c
timur <t.worck.m@gmail.com>
2022-12-01 18:56:13 +0100

    add InfoArray2 code

--- Lecture1/InfoArray2/Program.cs
+++ Lecture1/InfoArray2/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] colection)
+{
+    int length = colection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        colection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] colection, int find)
+{
+    int count = colection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (colection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+System.Console.WriteLine(pos);
