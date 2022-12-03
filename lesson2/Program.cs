b3ec78654eb7f33536888885996ed915f06b4e86
timur <t.worck.m@gmail.com>
2022-12-03 11:33:32 +0100

    add lesson2 code

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//9.Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//78 -> 8 
+//12-> 2 
+//85 -> 8
+
+int number = new Random().Next(10, 100); // Next (start, finish + 1)
+
+int firstDigit = number / 10;//Первая цифра числа: 78 -> 7
+
+int secondDigit = number % 10; //Вторая цифра числа: 78 -> 8
+
+int max = firstDigit; // Пусть максимум - первая цифра числа
+
+if (secondDigit > max) // max = 5, secondDigit  = 10
+{
+    max = secondDigit;
+}
+Console.WriteLine("Исходное число:" + number);
+Console.WriteLine("Максимальное число:" + max);
