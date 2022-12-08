f6514a7feeb0b2208fe320fa771ef209451c6308
timur <t.worck.m@gemail.com>
2022-12-08 17:11:26 +0100

    add inf about cycle while

--- Lecture3/Program.cs
+++ Lecture3/Program.cs
@@ -48,3 +48,19 @@ int result = Max(
 );
 Console.WriteLine(result);
 */
+//_____________________cycle while____________________________
+
+int[] array = { 1, 2, 3, 4, 5, 6, 4, 8, 9 };
+int n = array.Length;
+int index = 0;
+int find = 4;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
