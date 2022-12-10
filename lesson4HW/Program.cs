2ed00c1731121e28e505bf2619cf93e2616277fe
timur <t.worck.m@gemail.com>
2022-12-10 17:23:22 +0100

    add task 25

--- lesson4HW/Program.cs
+++ lesson4HW/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход 
+два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+int error = 0;
+void Exponentiation(int[] arr)
+{
+    int baseNumber = arr[0];
+    int degree = arr[1];
+    int rez = 1;
+
+    for (int i = 0; i < degree; i++)
+    {
+        rez *= baseNumber;
+    }
+
+    Console.WriteLine(rez);
+}
+
+Console.Write("Введите два числа x, y: ");
+int[] array = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+if (array.Length != 2)
+{
+    Console.WriteLine("Введено не верное количество символов!");
+    error = 1;
+}
+
+if (error == 0) Exponentiation(array);
+
+
