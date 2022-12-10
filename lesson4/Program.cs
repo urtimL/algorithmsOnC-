20c17061e01bfce07c42e40ff7d99131c411c435
timur <t.worck.m@gemail.com>
2022-12-10 16:23:32 +0100

    lesson4 code

--- lesson4/Program.cs
+++ lesson4/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World 4!");
+﻿// Напишите программу, которая принимает на вход число A 
+// и выдаёт сумму чисел от 1 до A 
+/*
+int GetSum(int A)
+{
+    int prod = 1;
+
+    for (int i = 1; i <= A; i++)
+    {
+        prod *= i;
+    }
+
+    return prod;
+}
+
+
+Console.Write("Введите число А: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetSum(N));
+/*
+int Count(int A)
+{
+    int rez = 0;
+
+    while (A != 0)
+    {
+        A = A / 10;
+        rez++;
+    }
+
+
+
+    return rez;
+}
+Console.Write("Введите число А: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Count(N));
+*/
+
+/*
+Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке. [1,0,1,1,0,1,0,0] 
+*/
+int[] GetArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+
+    return array;
+}
+string str = string.Join(", ", GetArray());
+Console.WriteLine("[" + str + "]");
+
+void PrintArray(int[] col)
+// Метод для выввода массива на экран 
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(" "); position++;
+    }
+}
