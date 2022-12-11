161b2ec5133648eabd335c5cbb6f5a846f7f850d
timur <t.worck.m@gemail.com>
2022-12-11 12:15:14 +0100

    add task 36

--- lesson5HW/Program.cs
+++ lesson5HW/Program.cs
@@ -4,7 +4,7 @@
 
 [345, 897, 568, 234] -> 2
 */
-
+/*
 int[] array = { 345, 897, 568, 234, 333, 850, 100, 888 };
 
 int NumberOfEven(int[] arr)
@@ -22,3 +22,33 @@ int NumberOfEven(int[] arr)
 }
 
 Console.WriteLine($"Количество четных чисел в массиве: [{String.Join("; ", array)}], равняется - {NumberOfEven(array)} шт.");
+*/
+//==============================================================================================================================
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+Console.Write("Для создания массива введите числа через запятую: ");
+int[] array = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+int SumElementsOddPositions(int[] arr)
+{
+    int length = arr.Length;
+    int rez = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            rez += arr[i];
+        }
+    }
+
+    return rez;
+}
+
+string text = "Сумма элементов, стоящих на нечетных позициях заданного массива";
+Console.WriteLine($"{text}, равна: {SumElementsOddPositions(array)}");
